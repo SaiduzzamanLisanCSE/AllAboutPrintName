30aa4f5b12a37ecd41b3b3cd303a1282830c1e69
saiduzzaman <lisan.saiduzzamancse@protonmail.com>
2024-10-17 16:27:11 +0600

    All about name

---
+++ name.py
@@ -0,0 +1,14 @@
+name = str(input("Enter your name: "))
+
+# remove whitespace from str
+name = name.strip()
+
+# capitalize => Auto capitalize a single word
+# name = name.capitalize()
+
+# Auto capitalize the first letter of each word
+name = name.title()
+
+# name = str(input("Enter your name: ")).strip().title()
+
+print("My name is", name)
